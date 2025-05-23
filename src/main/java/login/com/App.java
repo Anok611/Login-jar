96158fda704a47b22dc75a816147da938d005922
Anok611 <kibetanok@gmail.com>
2025-05-23 16:08:46 +0300

    Added a script
    Added a login line

--- src/main/java/login/com/App.java
+++ src/main/java/login/com/App.java
@@ -5,6 +5,6 @@ package login.com;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!, this is the Login Page");
     }
 }
